eb9e1dd2a3b2300aafeff981847ef75486875092
Agrim Manchanda <agrim.manchanda@hotmail.co.uk>
2019-03-22 19:48:13 +0000

    For merge

--- simple_compiler_tester/test_program.c
+++ simple_compiler_tester/test_program.c
@@ -1,15 +1,15 @@
 //multiple functions declared whic call each other
-int f() {
-    return 1;
-}
+// int f() {
+//     return 1;
+// }
 
-int g() {
-    return 3+f();
-}
+// int g() {
+//     return 3+f();
+// }
 
-int main () {
-    return 3*g();
-}
+// int main () {
+//     return 3*g();
+// }
 
 // int main () {
 //     int c = 3;
@@ -26,3 +26,18 @@ int main () {
 //     int a = b;
 //     return a;
 // }
+
+int f(int x, int y, int z){
+
+x = x-1;
+y = y+1;
+return x + y + z;
+}
+
+int main(){
+
+int output = f(10,10,10);
+
+return output;
+
+}
